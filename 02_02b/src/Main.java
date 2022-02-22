597d43e4a40381660dbd0d1a65f6181118e910c1
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-02-22 19:00:25 +0200

    primitive data types

--- 02_02b/src/Main.java
+++ 02_02b/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 18;
+        double studentGPA = 3.98;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'W';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println("Student name: " + studentFirstInitial + " " + studentLastInitial);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student GPA: " + studentGPA);
+
     }
 }
